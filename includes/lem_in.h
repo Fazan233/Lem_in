bd82ca03b84b72ea8029a1d4f25ede865ba1fa02
Vlad <vladuvi23@gmail.com>
2019-02-18 14:19:52 +0200

    created lem_in.h and added 7 structures to it

---
+++ includes/lem_in.h
@@ -0,0 +1,67 @@
+//
+// Created by Vladyslav USLYSTYI on 2019-02-18.
+//
+
+#ifndef LEM_IN_H
+#define LEM_IN_H
+
+typedef struct			s_lemin
+{
+	int					ants;
+	struct s_edge		*graph;
+	struct s_verticle	*vert;
+	struct s_mas_ways	*mas_ways;
+	struct s_verticle	*start_vert;
+	struct s_verticle	*end_vert;
+}						t_lemin;
+
+typedef struct			s_verticle
+{
+	char				*name;
+	struct s_verticle	*next;
+	struct s_verticle	*prev;
+	struct s_edge		*short_way;
+	int					weight;
+	int					x;
+	int					y;
+}						t_verticle;
+
+typedef struct			s_edge
+{
+	struct s_verticle	*a;
+	struct s_verticle	*b;
+	int					name;
+	int					available;
+	struct s_edge		*next;
+}						t_edge;
+
+typedef struct			s_way
+{
+	int					len;
+	struct s_verticle	*vert;
+	struct s_way		*next;
+	struct s_way		*prev;
+	int					ant;
+}						t_way;
+
+typedef struct			s_ways
+{
+	struct s_way		*way;
+	struct s_ways		*next;
+}						t_ways;
+
+typedef struct			s_mas_ways
+{
+	int					iter;
+	struct s_ways		*ways;
+	struct s_mas_ways	*naxt;
+}						t_mas_ways;
+
+typedef struct			s_used_vert
+{
+	struct s_verticle	*vert;
+	struct s_used_vert	*next;
+}						t_used_vert;
+
+
+#endif
